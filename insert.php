834d5a1e2a0930434e9f5ff4a9324eaaf05426ee
it63070045 <73681882+it63070045@users.noreply.github.com>
2020-11-20 23:57:43 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -18,7 +18,7 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
-    window.location.replace("index.php");
+    header('Location: index.php');
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
