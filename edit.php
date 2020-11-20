dfe4ef620ab7e4eadc2220a242611d7c659fc311
it63070045 <73681882+it63070045@users.noreply.github.com>
2020-11-20 23:52:19 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,9 +1,9 @@
 <?php
-	$conn = mysqli_connect('putthimafah.mysql.database.azure.com', 'putthima@putthimafah', 'fhpthms954*', 'itflab');
+	$conn = mysqli_connect( 'natanon.mysql.database.azure.com', 'NATANON@natanon', 'gG6tbrvqmn', 'itflab', 3306);
 
 	$id = $_GET['id'];
 
-	$sql = 'SELECT * FROM guestbook WHERE id = '.$id.'';
+	$sql = 'SELECT * FROM guestbook WHERE ID = '.$id.'';
 	$query = mysqli_query($conn, $sql);
 	if(!$query) {
 		header('Location: show.php');
@@ -19,13 +19,13 @@
 </head>
 <body>
   <form action = "update.php" method = "post" >
-  <input type="text" name="id" value="<?php echo $data['id']; ?>" class="form-control d-none" required>
+  <input type="text" name="id" value="<?php echo $data['ID']; ?>" class="form-control d-none" required>
     Name:<br>
-    <input type="text" name = "name" id="idName" placeholder="Enter Name" value="<?php echo $data['name']; ?>"> <br>
+    <input type="text" name = "name" id="idName" placeholder="Enter Name" value="<?php echo $data['Name']; ?>"> <br>
     Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"><?php echo $data['comment']; ?></textarea><br>  
+    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"><?php echo $data['Comment']; ?></textarea><br>  
     Link:<br>
-    <input type="text" name = "link" id="idLink" placeholder="Enter Link" value="<?php echo $data['link']; ?>"> <br><br>
+    <input type="text" name = "link" id="idLink" placeholder="Enter Link" value="<?php echo $data['Link']; ?>"> <br><br>
     <input type="submit" id="commentBtn">
   </form>
 </body>
