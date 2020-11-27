91c681822d2bc3baaff5c533f4a6d34ca98cda26
it63070045 <73681882+it63070045@users.noreply.github.com>
2020-11-27 12:56:49 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
   
 <?php
     $conn = mysqli_init();
-    mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070045_test', 'NGUcvk58', 'testtable', 3306);
+    mysqli_real_connect($conn, 'natanon.mysql.database.azure.com', 'NATANON@natanon', 'gG6tbrvqmn', 'guestbook', 3306);
 
     if (mysqli_connect_errno($conn)) {
         die('Failed to connect to MySQL: ' . mysqli_connect_error());
